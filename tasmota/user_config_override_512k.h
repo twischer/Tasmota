fb55ab8fe945ea6de1a0ffaf021f65f9286e9b37
Timo Wischer <twischer@freenet.de>
2021-02-13 08:46:17 +0100

    Build for 512kB flash
    $ pio run -e tasmota-lite -t upload
    
    In case the firmware is still to big have a look in
    https://github.com/arendst/Tasmota/blob/development/BUILDS.md
    and disable additional features which are not required.
    Disable only features which are also disabled for tasmota-minimal.
    
    Signed-off-by: Timo Wischer <twischer@freenet.de>

---
+++ tasmota/user_config_override_512k.h
@@ -0,0 +1,32 @@
+/*
+  user_config_override.h - user configuration overrides my_user_config.h for Tasmota
+
+  Copyright (C) 2021  Theo Arends
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef _USER_CONFIG_OVERRIDE_H_
+#define _USER_CONFIG_OVERRIDE_H_
+
+// force the compiler to show a warning to confirm that this file is included
+#warning **** user_config_override.h: Using Settings from this File ****
+
+#undef USE_EMULATION
+#undef USE_EMULATION_HUE
+#undef USE_EMULATION_WEMO
+#undef USE_TIMERS
+//#undef USE_BL0940
+
+#endif  // _USER_CONFIG_OVERRIDE_H_
